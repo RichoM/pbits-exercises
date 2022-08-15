048387d826b7946c68abd321b38b16e82edca3d7
RichoM <richi.moran@gmail.com>
2022-08-15 00:36:22 -0300

    Excluding the solutions folder from the build

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 rmdir /S /Q "public/main/js"
 call shadow-cljs release main
-call electron-packager . Ejercicios --out=out --overwrite --ignore="\.(shadow-cljs|calva|clj-kondo)"
+call electron-packager . Ejercicios --out=out --overwrite --ignore="solutions|\.(shadow-cljs|calva|clj-kondo)"
